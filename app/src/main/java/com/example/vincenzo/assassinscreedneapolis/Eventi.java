ef5186430069c2c741145de504e939f2c851b9d7
vinsan <v.santoro21@studenti.unisa.it>
2018-01-29 23:04:32 +0100

    Support for sending e-mail as Staff member and as viewer only

--- app/src/main/java/com/example/vincenzo/assassinscreedneapolis/Eventi.java
+++ app/src/main/java/com/example/vincenzo/assassinscreedneapolis/Eventi.java
@@ -1,6 +1,8 @@
 package com.example.vincenzo.assassinscreedneapolis;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,15 +18,25 @@ public class Eventi extends AppCompatActivity {
         setContentView(R.layout.activity_eventi);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        String NomeCognome = prefs.getString("NomeCognome", "null");
-        String numeroTessera = prefs.getString("Tessera", "null");
+        final String NomeCognome = prefs.getString("NomeCognome", "null");
+        final String numeroTessera = prefs.getString("Tessera", "null");
         String npg1 = prefs.getString("pg1", "null");
         String npg2 = prefs.getString("pg2", "null");
         String npg3 = prefs.getString("pg3", "null");
-        String fb = prefs.getString("Facebook", "null");
-        String cellulare = prefs.getString("Cellulare", "null");
+        final String fb = prefs.getString("Facebook", "null");
+        final String cellulare = prefs.getString("Cellulare", "null");
         //SharedPreferences.Editor editor = prefs.edit();
-        String mail = "acn.soa.iscrizioneeventi@gmail.com";    //<- TARGET EMAIL
+        final String mail = "acn.soa.iscrizioneeventi@gmail.com";    //<- TARGET EMAIL
+        //PARAMETRI DA AGGIORNARE OGNI MESE PER MODIFICARE L'EVENTO
+        final String nomeEvento = "Taverna";
+        final String dataEvento = "13 Febbraio 2018";
+
+        /*Nome socio: Vincenzo Santoro
+Nome pg: Nessuno
+Tipo di partecipazione: Staff
+Numero tessera:
+Numero cellulare: (il tuo)
+Indirizzo facebook: (il tuo)*/
 
         Button button3 = findViewById(R.id.button3);
         button3.setText("Iscrizione PG");
@@ -34,9 +46,21 @@ public class Eventi extends AppCompatActivity {
 
         Button button5 = findViewById(R.id.button5);
         button5.setText("Iscrizione Staff");
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iscrizioneNoPG(NomeCognome, "Staff", numeroTessera, cellulare, fb, mail, nomeEvento, dataEvento);
+            }
+        });
 
         Button button6 = findViewById(R.id.button6);
         button6.setText("Iscrizione Spettatore");
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iscrizioneNoPG(NomeCognome, "Spettatore", numeroTessera, cellulare, fb, mail, nomeEvento, dataEvento);
+            }
+        });
 
         Button button7 = findViewById(R.id.button7);
         button7.setText("Iscrizione PG in Prova");
@@ -53,6 +77,17 @@ public class Eventi extends AppCompatActivity {
                 finish();
             }
         });
-
     }
+
+    public void iscrizioneNoPG(String nomeCognome, String tipoPartecipazione, String numeroTessera, String cellulare, String fb, String mail, String nomeEvento, String dataEvento){
+        Intent intent = new Intent(android.content.Intent.ACTION_SENDTO);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Iscrizione " + tipoPartecipazione);
+        intent.putExtra(Intent.EXTRA_TEXT, "Nome socio: "+ nomeCognome +"\n" + "Nome pg: Nessuno\n" + "Tipo di partecipazione: "+ tipoPartecipazione +"\n" + "Numero tessera:" + numeroTessera + "\n" + "Numero cellulare: "+ cellulare +"\n" + "Indirizzo facebook: "+ fb + "\n Nome Evento: " + nomeEvento+"\n Data Evento: "+dataEvento);
+        intent.setData(Uri.parse("mailto:"+mail));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+
+        startActivity(intent);
+    }
+
 }
